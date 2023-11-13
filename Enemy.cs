1a4cde29530a659fd20f135db90f26ac09ed54ed
Poligorn <133873347+Poligorn@users.noreply.github.com>
2023-11-13 12:05:40 +0300

    Add files via upload

--- Enemy.cs
+++ Enemy.cs
@@ -11,8 +11,14 @@ public class Enemy : MonoBehaviour
     public float shootInterval = 1f;
     public float shootTimer = 0f;
 
+    public float speedPerLevel = 0.2f;
+
+    public int price;
+
     private void Start()
     {
+        speed += speedPerLevel * LevelController.level;
+
         borderPosX = Corn.singleton.transform.position.x;
     }
 
@@ -48,12 +54,20 @@ public class Enemy : MonoBehaviour
         health -= 1;
 
         if (health <= 0)
-            Destroy(gameObject);
+        {
+            Die();
+        }
     }
 
     public void Attack()
     {
         Corn.singleton.TakeDamage();
+    }
+
+    public void Die()
+    {
+        Corn.singleton.AddCrystals(price);
+        Destroy(gameObject);
     }
 
 }
