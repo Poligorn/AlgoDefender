f3108e2da75de92025e4ffe7a31f637195aee900
Poligorn <133873347+Poligorn@users.noreply.github.com>
2023-11-13 12:01:03 +0300

    Add files via upload

--- Corn.cs
+++ Corn.cs
@@ -6,15 +6,37 @@ public class Corn : MonoBehaviour
 {
     public static Corn singleton;
     public int health;
+    public int crystals;
+
+    public int initHealth;
+    public int healthPerUpgrade;
 
     private void Awake()
     {
         singleton = this;
+        int healthBonus = healthPerUpgrade * PlayerPrefs.GetInt("healthGrade", 0);
+        health = initHealth + healthBonus;
+        crystals = PlayerPrefs.GetInt("crystals", 0);
     }
 
     public void TakeDamage()
     {
-        health -= 1;
+        if(health > 0)
+        {
+            health -= 1;
+        }
+    }
+
+    public void AddCrystals(int newCrystals)
+    {
+        crystals += newCrystals;
+        GameController.SaveCrystals();
+    }
+
+    public void DecCrystals(int soldCrystals)
+    {
+        crystals -= soldCrystals;
+        GameController.SaveCrystals();
     }
 
 }
